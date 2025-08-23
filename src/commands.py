de0b38f11cc18b0b165e19ff2eb9016eaee5cacc
XiaoLing-git <samuel.ling.xiao@qq.com>
2025-08-23 17:22:35 +0800

    add AnalogChannelRangeAddress

--- src/commands.py
+++ src/commands.py
@@ -20,10 +20,29 @@ class AnalogChannelAddress(Enum):
     ch4 = "00030001"
 
 
-class IoMode(Enum):
-    """Pin IO mode options."""
+class AnalogChannelRangeAddress(Enum):
+    """Analog channel range read address."""
+
+    all = "00C80004"
+    ch1 = "00C80001"
+    ch2 = "00C90001"
+    ch3 = "00CA0001"
+    ch4 = "00CB0001"
+
+
+class InputIoMode(Enum):
+    """Pin input IO mode options."""
 
     Normal = "01"
     RiseEdge = "02"
     FallEdge = "03"
     Count = "04"
+
+
+class OutputIoMode(Enum):
+    """Pin output IO mode options."""
+
+    Normal = "01"
+    RiseEdgeDelay = "02"
+    FallEdgeDelay = "03"
+    Pulse = "04"
