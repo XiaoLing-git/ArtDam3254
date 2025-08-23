5be5ba112f52f45326de8b1af0898a69bbdfab50
XiaoLing-git <samuel.ling.xiao@qq.com>
2025-08-23 16:35:53 +0800

    add FunctionCode

---
+++ src/commands.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class FunctionCode(Enum):
+    """base function code statement"""
+
+    InputRead = "04"
+    StateRead = "03"
+    SingleWrite = "06"
+    MultiWrite = "10"
