fdaa3b6238aba6bc0b06c993b0d978af18120a6c
XiaoLing-git <samuel.ling.xiao@qq.com>
2025-08-23 17:06:06 +0800

    add IoMode & AnalogChannelAddress

--- src/commands.py
+++ src/commands.py
@@ -8,3 +8,22 @@ class FunctionCode(Enum):
     StateRead = "03"
     SingleWrite = "06"
     MultiWrite = "10"
+
+
+class AnalogChannelAddress(Enum):
+    """Analog channel read address."""
+
+    all = "00000004"
+    ch1 = "00000001"
+    ch2 = "00010001"
+    ch3 = "00020001"
+    ch4 = "00030001"
+
+
+class IoMode(Enum):
+    """Pin IO mode options."""
+
+    Normal = "01"
+    RiseEdge = "02"
+    FallEdge = "03"
+    Count = "04"
